930e5a7755e0ee025dab00d6d9463136543fe193
tmu <t.man.atl.2012@gmail.com>
2023-02-04 12:04:32 -0800

    First function commit with uart

--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,8 @@
 
 use core::panic::PanicInfo;
 
+use core::arch::global_asm;
+
 pub mod uart;
 
 #[macro_export]
@@ -33,6 +35,39 @@ macro_rules! println
 fn panic(_info: &PanicInfo) -> ! {
     loop {}
 }
+
+// qemu -kernel loads the kernel at 0x80000000
+// and causes each hart (i.e. CPU) to jump there.
+// kernel.ld causes the following code to
+// be placed at 0x80000000.
+
+// set up a stack for C.
+// stack0 is declared in start.c,
+// with a 4096-byte stack per CPU.
+// sp = stack0 + (hartid * 4096)
+
+// # jump to start() in start.c
+
+// global_asm!(
+    // ".section .data",
+	    // ".global _stack0",
+	    // "_stack0:",
+	    // ".int 0x9000000",
+	    // ".section .text",
+// 	    "	.global _entry",
+// 	    "	.extern _start",
+// 	    "	_entry:",
+// 	    "    la sp, _stack0",
+// 	    "        li a0, 1024*4",
+// 	    "        csrr a1, mhartid",
+// 	    "        addi a1, a1, 1",
+// 	    "        mul a0, a0, a1",
+// 	    "        add sp, sp, a0",
+// 	    "    call _start",
+// 	    "	spin:",
+// 	    "    j spin"
+// );
+// global_asm!(include_str!("entry.S"));
 
 #[no_mangle]
 pub extern "C" fn _start() -> ! {
