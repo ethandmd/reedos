05f0a732f5aa875538a9e8b9831626e8c03c2087
ethandmd <ethandmd@protonmail.com>
2023-02-03 23:57:06 -0800

    Add uart driver

--- src/main.rs
+++ src/main.rs
@@ -3,24 +3,44 @@
 
 use core::panic::PanicInfo;
 
+pub mod uart;
+
+#[macro_export]
+macro_rules! print
+{
+	($($args:tt)+) => ({
+			use core::fmt::Write;
+			let _ = write!(crate::uart::Uart::new(0x1000_0000), $($args)+);
+			});
+}
+
+#[macro_export]
+macro_rules! println
+{
+	() => ({
+		   print!("\r\n")
+		   });
+	($fmt:expr) => ({
+			print!(concat!($fmt, "\r\n"))
+			});
+	($fmt:expr, $($args:tt)+) => ({
+			print!(concat!($fmt, "\r\n"), $($args)+)
+			});
+}
+
 // The never type "!" means diverging function (never returns).
 #[panic_handler]
 fn panic(_info: &PanicInfo) -> ! {
     loop {}
 }
 
-static HELLO: &[u8] = b"Hello World!";
-
 #[no_mangle]
 pub extern "C" fn _start() -> ! {
-    let vga_buffer = 0xb8000 as *mut u8;
 
-    for (i, &byte) in HELLO.iter().enumerate() {
-        unsafe {
-            *vga_buffer.offset(i as isize * 2) = byte;
-            *vga_buffer.offset(i as isize * 2 + 1) = 0xb;
-        }
-    }
+    let mut myuart = uart::Uart::new(0x1000_0000);
+    myuart.init();
+
+    println!("MELLOW SWIRLED!");
 
     loop {}
 }
