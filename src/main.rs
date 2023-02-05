4148001bb66185a9260753fea96214c64cb6ee05
ethandmd <ethandmd@protonmail.com>
2023-02-04 23:10:51 -0800

    Start function now mret to main fn

--- src/main.rs
+++ src/main.rs
@@ -5,6 +5,7 @@ use core::panic::PanicInfo;
 
 pub mod uart;
 pub mod entry;
+pub mod riscv;
 
 #[macro_export]
 macro_rules! print
@@ -35,13 +36,75 @@ fn panic(_info: &PanicInfo) -> ! {
     loop {}
 }
 
+// Have to get the timer interrupts that arrive in mach mode
+// and convert to s/w interrupts for trap.
+fn timerinit() {
+//    let hartid = riscv::read_mhartid();
+//    let interval = 1000000; // <- # no. cycles ~ 1/10 sec in qemu.
+//    let mtcmp = riscv::clint_mtimecmp(hartid);
+    // TODO
+}
+
+// Referenced from xv6-riscv/kernel/start.c:
+// Here, we are going to perform set up in machine mode.
+// However, we are going to return (to main) via mret (riscv).
 #[no_mangle]
-pub extern "C" fn _start() -> ! {
+pub extern "C" fn _start() {
+    let fn_main = main as *const ();
 
-    let mut myuart = uart::Uart::new(0x1000_0000);
-    myuart.init();
+    let mut uartd = uart::Uart::new(0x1000_0000);
+    uartd.init();
+    println!("[INFO]: Currently on hartid: {}", riscv::read_mhartid());
+   
+    // Set the *prior* privilege mode to supervisor.
+    // Bits 12, 11 are for MPP. They are WPRI.
+    // For sstatus we can write SPP reg, bit 8.
+    let mut ms = riscv::read_mstatus();
+    ms &= !riscv::MSTATUS_MPP_MASK;
+    ms |= riscv::MSTATUS_MPP_S; 
+    riscv::write_mstatus(ms);
 
-    println!("MELLOW SWIRLED!");
+    // Set machine exception prog counter to 
+    // our main function for later mret call.
+    println!("[INFO]: main fn's addr?: {:?}", fn_main);
+    riscv::write_mepc(fn_main);
+
+    // Disable paging while setting up.
+    riscv::write_satp(0);
+
+    // Allow our kernel to handle interrupts from sup mode
+    // by "delegating" interrupts and exceptions.
+    riscv::write_medeleg(0xffff); // Check 3.1.8 in: (haven't read it in full yet)
+    riscv::write_mideleg(0xffff); // https://five-embeddev.com/riscv-isa-manual/latest/machine.html#machine
+    riscv::write_sie(
+        riscv::read_sie() | riscv::SIE_SEIE | riscv::SIE_STIE | riscv::SIE_SSIE
+    );
+
+    // Now give sup mode access to (all??) of phys mem.
+    // Check 3.1.6 of line 66 link.
+    riscv::write_pmpaddr0(0x3fffffffffffff as u64); // Prayers that ULL == u64
+    riscv::write_pmpcfg0(0xf);
+
+    // Get interrupts from clock, handled by timerinit().
+    timerinit();
+
+    // Store each hart's hartid in its tp reg for identification.
+    let hartid = riscv::read_mhartid();
+    riscv::write_tp(hartid);
+
+    // Now return to sup mode and jump to main().
+    riscv::call_mret();
+
+}
+
+// Doesn't need to be extern C, no_mangle, nothin' fancy...?
+fn main() -> ! {
+    // Init uart driver.
+    let mut uartd = uart::Uart::new(0x1000_0000);
+    uartd.init();
+    // Seasons greetings.
+    println!("MELLOW SWIRLED!\n from,\n your fav main fn");
+    println!("(called from _start fn!)");
 
     loop {}
 }
---
+++ src/riscv.rs
@@ -0,0 +1,245 @@
+// All referenced from xv6-riscv/kernel/riscv.h
+
+use core::arch::asm;
+
+// MPP := Machine previous protection mode.
+pub const MSTATUS_MPP_MASK: u64 = 3 << 11; // Mask for bit tricks
+pub const MSTATUS_MPP_M: u64 = 3 << 11; // Machine
+pub const MSTATUS_MPP_S: u64 = 1 << 11; // Supervisor
+pub const MSTATUS_MPP_U: u64 = 0 << 11; // User
+pub const MSTATUS_MIE: u64 = 1 << 3; // machine-mode interrupt enable.
+
+// sstatus := Supervisor status reg.
+pub const SSTATUS_SPP: u64 = 1 << 8;  // Previous mode, 1=Supervisor, 0=User
+pub const SSTATUS_SPIE: u64 = 1 << 5; // Supervisor Previous Interrupt Enable
+pub const SSTATUS_UPIE: u64 = 1 << 4; // User Previous Interrupt Enable
+pub const SSTATUS_SIE: u64 = 1 << 1;  // Supervisor Interrupt Enable
+pub const SSTATUS_UIE: u64 = 1 << 0;  // User Interrupt Enable
+
+// Machine-mode Interrupt Enable
+pub const MIE_MEIE: u64 = 1 << 11; // external
+pub const MIE_MTIE: u64 = 1 << 7; // timer
+pub const MIE_MSIE: u64 = 1 << 3;  // software
+
+// Supervisor Interrupt Enable
+pub const SIE_SEIE: u64 = 1 << 9; // external
+pub const SIE_STIE: u64 = 1 << 5; // timer
+pub const SIE_SSIE: u64 = 1 << 1; // software
+
+// CLINT := Core local interruptor (where the timer is).
+pub const CLINT: u64 = 0x2000000; // clint is at this location in memlayout.
+// #define CLINT_MTIMECMP(hartid) (CLINT + 0x4000 + 8*(hartid))
+pub fn clint_mtimecmp(id: u64) -> u64 {
+    CLINT + 0x4000 + 8 * id
+}
+// #define  CLINT_MTIME (CLINT + 0xBFF8) // cycles since boot.
+pub fn clint_mtime() -> u64 {
+    CLINT + 0xBFF8
+}
+
+
+// Return id of current hart.
+// the "m" in "mstatus" means machine mode.
+// Note mstatus -> sstatus reg for supervisor mode.
+pub fn read_mhartid() -> u64 {
+    let id: u64;
+    // Volatile by default?
+    unsafe {
+        asm!("csrr {}, mhartid", out(reg) id);
+    }
+    id
+}
+
+// Read CSR := Control and Status Register mstatus.
+// Refer to chap 9 of riscv isa manual for info on CSRs.
+pub fn read_mstatus() -> u64 {
+    let status: u64;
+    unsafe {
+        asm!("csrr {}, mstatus", out(reg) status);
+    }
+    status
+}
+
+// Write to mstatus.
+pub fn write_mstatus(status: u64) {
+    unsafe {
+        asm!("csrw mstatus, {}", in(reg) status);
+    }
+}
+
+// Set mepc := machine exception program counter.
+// a.k.a. what instr (address) to go to from exception.
+pub fn write_mepc(addr: *const ()) {
+    unsafe {
+        asm!("csrw mepc, {}", in(reg) addr);
+    }
+}
+
+pub fn read_sstatus() -> u64 {
+    let status: u64;
+    unsafe {
+        asm!("csrr {}, sstatus", out(reg) status);
+    }
+    status
+}
+
+pub fn write_status(status: u64) {
+    unsafe {
+        asm!("csrw sstatus, {}", in(reg) status);
+    }
+}
+
+// Enable sup mode interrupt and exception. 
+pub fn read_sip() -> u64 {
+    let x: u64;
+    unsafe {
+        asm!("csrr {}, sip", out(reg) x);
+    }
+    x
+}
+
+pub fn write_sip(ire: u64) {
+    unsafe {
+        asm!("csrw sip, {}", in(reg) ire);
+    }
+}
+
+pub fn read_sie() -> u64 {
+    let x: u64;
+    unsafe {
+        asm!("csrr {}, sie", out(reg) x);
+    }
+    x
+}
+
+pub fn write_sie(ire: u64) {
+    unsafe {
+        asm!("csrw sie, {}", in(reg) ire);
+    }
+}
+
+pub fn read_mie() -> u64 {
+    let x: u64;
+    unsafe {
+        asm!("csrr {}, mie", out(reg) x);
+    }
+    x
+}
+
+pub fn write_mie(x: u64) {
+    unsafe {
+        asm!("csrw mie, {}", in(reg) x);
+    }
+}
+
+
+// SATP := supervisor address translation and protection.
+// This is where we hold the page table address.
+// use riscv's sv39 page table scheme.
+//
+// For reference:
+// #define SATP_SV39 (8L << 60)
+// #define MAKE_SATP(pagetable) (SATP_SV39 | (((uint64)pagetable) >> 12))
+pub fn read_satp() -> u64 {
+    let pt: u64;
+    unsafe {
+        asm!("csrr {}, satp", out(reg) pt);
+    }
+    pt
+}
+
+ pub fn write_satp(pt: u64) {
+     unsafe {
+         asm!("csrw satp, {}", in(reg) pt);
+     }
+ }
+
+// medeleg := machine exception delegation (to supervisor mode)
+// mideleg := machine interrupt delegation (to supervisor mode)
+pub fn read_medeleg() -> u64 {
+    let med: u64;
+    unsafe {
+        asm!("csrr {}, medeleg", out(reg) med);
+    }
+    med
+}
+
+pub fn write_medeleg(med: u64) {
+    unsafe {
+        asm!("csrw medeleg, {}", in(reg) med);
+    }
+}
+
+pub fn read_mideleg() -> u64 {
+    let mid: u64;
+    unsafe {
+        asm!("csrr {}, mideleg", out(reg) mid);
+    }
+    mid
+}
+
+pub fn write_mideleg(mid: u64) {
+    unsafe {
+        asm!("csrw mideleg, {}", in(reg) mid);
+    }
+}
+
+// pmpaddr := phys mem protection addr. 
+// Configure to give supervisor mode access to
+// certain parts of memory.
+pub fn write_pmpaddr0(addr: u64) {
+    unsafe {
+        asm!("csrw pmpaddr0, {}", in(reg) addr);
+    }
+}
+
+pub fn write_pmpcfg0(addr: u64) {
+    unsafe {
+        asm!("csrw pmpcfg0, {}", in(reg) addr);
+    }
+}
+
+// Just for curiosity's sake:
+// https://github.com/rust-lang/rust/issues/82753
+// 
+// tp := thread pointer?
+// This way we can query a hart's hartid and store it in tp reg.
+pub fn write_tp(id: u64) {
+    unsafe {
+        asm!("mv tp, {}", in(reg) id);
+    }
+}
+
+// Make sure mret has an addr to go to!
+pub fn call_mret() {
+    unsafe {
+        asm!("mret");
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
