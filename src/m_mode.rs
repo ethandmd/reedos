1d10f3d3f6c97396432455985e7b84f456f716de
Dylan McNamee <dylan.mcnamee@gmail.com>
2023-02-04 19:41:17 -0800

    fixed retab

--- src/m_mode.rs
+++ src/m_mode.rs
@@ -3,31 +3,31 @@ pub mod m_mode {
     use core::arch::asm;
 
     fn set_vec(vec: &fn ()) {
-    let addr: isize = vec as isize;
-    unsafe {
-        asm!("csrw mvec, {ad}",
-         ad = in(reg) addr);
-    }
+        let addr: isize = vec as isize;
+        unsafe {
+            asm!("csrw mvec, {ad}",
+             ad = in(reg) addr);
+        }
     }
 
     fn trap_get_cause() -> u64 {
-    let mut output: u64 = 0;
-    unsafe {
-        asm!("csrrc {op}, mcause, x0",
-         op = out(reg) output)
-    }
-    return output;
+        let mut output: u64 = 0;
+        unsafe {
+            asm!("csrrc {op}, mcause, x0",
+             op = out(reg) output)
+        }
+        return output;
     }
 
     fn trap_vec () {
-    let cause: u64 = trap_get_cause();
+        let cause: u64 = trap_get_cause();
 
-    println!("Machine mode trap fired: Cause {}", cause);
+        println!("Machine mode trap fired: Cause {}", cause);
 
     }
 
 
     pub fn setup_trap() {
-    set_vec(trap_vec);
+        set_vec(trap_vec);
     }
 }
