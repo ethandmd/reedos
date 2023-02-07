2d0f98eeedf57447172a51e36823a97717d8fa0c
ethandmd <ethandmd@protonmail.com>
2023-02-07 15:56:55 -0800

    Add mutexguard with drop-unlock

--- src/spinlock.rs
+++ src/spinlock.rs
@@ -5,33 +5,38 @@
 use core::cell::UnsafeCell;
 use core::sync::atomic::*;
 
-// Use unit struct CriticalSection as a lifetime
-// reference for holding on to a mutex.
-pub struct CriticalSection;
+pub struct MutexGuard<'a, T> {
+    mutex: &'a Mutex<T>,
+}
 
-impl CriticalSection {
-    pub fn new() -> Self {
-        CriticalSection
+impl<T> core::ops::Deref for MutexGuard<'_, T> {
+    type Target = T;
+    fn deref(&self) -> &T {
+        unsafe { &*self.mutex.inner.get() }
+    }
+}
+
+impl<T> core::ops::DerefMut for MutexGuard<'_, T> {
+    fn deref_mut(&mut self) -> &mut T {
+        unsafe { &mut *self.mutex.inner.get() }
+    }
+}
+
+impl<T> core::ops::Drop for MutexGuard<'_, T> {
+    fn drop(&mut self) {
+        self.mutex.lock_state.store(0, Ordering::Release);
     }
 }
 
 // Simple mutex implementation.
 // 1. Try to acquire mutex for critical section.
-// 2. If unable, spin for a bit.
-// 3. If still unable, sleep.
-// 4. -> 1.
-//
-// We'll use similar Mutex -> MutexGuard construction,
-// but instead of a guard with Deref and Drop traits,
-// we'll have lock() directly return a ptr to T with inner.get().
-// I know, living on the edge. BUT, we use the unit struct 
-// critical section as a lifetime for T, thus it is safe...right?
+// 2. If unable, spin.
 pub struct Mutex<T> {
     lock_state: AtomicU32, // (0,1) = (unlocked, locked)
-    inner: UnsafeCell<T>, // Not using SyncUnsafeCell bc don't feel like using nightly.
+    inner: UnsafeCell<T>, 
 }
 
-unsafe impl<T: Send> Sync for Mutex<T> {}
+//unsafe impl<T: Send> Sync for Mutex<T> {}
 
 impl<T> Mutex<T> {
     // https://doc.rust-lang.org/reference/const_eval.html
@@ -43,14 +48,15 @@ impl<T> Mutex<T> {
     }
 
     // Still debating just doing this in asm.
-    pub fn lock<'crit>(&self, _crit: &'crit CriticalSection) -> &'crit T {
+    pub fn lock(&self) -> MutexGuard<T> {
         // Use Acquire memory order to load lock value.
-        // Yes, this is an indefinite spin. 
         // TODO:
-        // Do not indefinitely spin.
+        // Spin loop improvement.
         while self.lock_state.swap(1, Ordering::Acquire) == 1 {}
-        unsafe { &*self.inner.get() }
+        MutexGuard { mutex: self }
     }
+
+    
 }
 
 
