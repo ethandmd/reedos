60d59db83632f44ca2c634d1364bb8f0cb81f013
ethandmd <ethandmd@protonmail.com>
2023-02-19 14:17:19 -0800

    Add kpools init with linker provided start and end symbols

--- src/vm.rs
+++ src/vm.rs
@@ -1,2 +1,9 @@
 pub mod palloc;
 pub mod ptable;
+
+extern "C" {
+    static _page_start: usize;
+    static _page_end: usize;
+}
+
+static mut POOL: palloc::Kpools = palloc::Kpools::new(_page_start, _page_end);
