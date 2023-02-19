cf23e19992a5345e3b509da9076489fd709c2888
ethandmd <ethandmd@protonmail.com>
2023-02-19 14:52:34 -0800

    Update linker symbols

--- src/entry.rs
+++ src/entry.rs
@@ -37,7 +37,7 @@ global_asm!(
         li t0, 0x1000 # = 4096
         li t1, 0x2 # For param::NHART == 2...this is unstable.
         mul t0, t0, t1 # 4096 * NHART
-        la sp, end
+        la sp, __bss_end
         add sp, sp, t0 # Setup stack ptr at offset + end of .bss
 
         # Add 4k guard page per hart
