776b1fe9c95630c8f6de4336bb222122064ebb9e
ethandmd <ethandmd@protonmail.com>
2023-02-19 16:24:26 -0800

    Make linker symbols public

--- src/vm.rs
+++ src/vm.rs
@@ -2,8 +2,6 @@ pub mod palloc;
 pub mod ptable;
 
 extern "C" {
-    static _page_start: usize;
-    static _page_end: usize;
+    pub static __bss_end: usize;
+    pub static __memory_end: usize;
 }
-
-static mut POOL: palloc::Kpools = palloc::Kpools::new(_page_start, _page_end);
