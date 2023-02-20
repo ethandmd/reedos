8d4e96d73828c93d936d83cf9ae010df15f480f6
ethandmd <ethandmd@protonmail.com>
2023-02-20 14:22:30 -0800

    Add kernel page table init

--- src/vm.rs
+++ src/vm.rs
@@ -1,7 +1,16 @@
 pub mod palloc;
 pub mod ptable;
 
-extern "C" {
-    pub static __bss_end: usize;
-    pub static __memory_end: usize;
+use palloc::Kpools;
+use ptable::kpage_init;
+use crate::hw::param::{BSS_END, DRAM_END};
+
+pub fn init() {
+    // Setup page allocation pool for harts + global
+    let bss_end = unsafe { BSS_END };
+    let mem_end = unsafe { DRAM_END };
+    let mut pool = Kpools::new(bss_end, mem_end);
+
+    // Map text, data, heap into kernel memory
+    let kpage_root = kpage_init(&mut pool);
 }
