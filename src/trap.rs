b44bc99db2852f1ec3926aca3045f441b0a0e32b
tmu <t.man.atl.2012@gmail.com>
2023-02-22 13:57:59 -0800

    Sup mode interupts? not clearing bit.

--- src/trap.rs
+++ src/trap.rs
@@ -13,16 +13,28 @@ extern "C" {
 }
 
 pub fn init() {
-    // riscv::write_stvec(__strapvec as usize);
+    riscv::write_stvec(__strapvec as usize);
 }
 
-// fn handler() {
-//     println!("#");
-// }
+fn s_handler() {
+    log!(Debug, "Start of sup mode handler");
+    let cause = riscv::read_scause();
+
+
+    // scause 5 is timer interupt
+    match cause {
+        _ => {
+            log::log!(Warning, "Uncaught supervisor mode interupt. scause: {:X}", cause);
+        }
+    }
+
+    riscv::write_sip(riscv::read_sip() & !5);
+}
 
 fn m_handler() {
     let mcause = riscv::read_mcause();
 
+    log!(Debug, "Machine mode interupt. Are you sure you don't want a Supervisor mode one?");
     match mcause {
         riscv::MSTATUS_TIMER => {
             log::log!(Debug, "Timer interupt, hart: {}", riscv::read_mhartid());
@@ -31,10 +43,7 @@ fn m_handler() {
         _ => {
             log::log!(Warning, "Uncaught machine mode interupt. mcause: {:X}", mcause);
         }
-        
     }
-
-    //uart::Uart::new().lock().put(". ".bytes().next().unwrap());
 }
 
 global_asm!(
@@ -43,8 +52,6 @@ global_asm!(
     .align 4
     .global __mtrapvec
 __mtrapvec:
-    .globl __strapvec
- __strapvec:
     call {}
     li a1, 2
     csrw sip, a1
@@ -52,80 +59,80 @@ __mtrapvec:
     "#, sym m_handler
     );
 
-// global_asm!(
-//     r#"
-//     .option norvc
-//     .align 4
-//     .globl __strapvec
-//  __strapvec:
-//     addi sp, sp, -256
+global_asm!(
+    r#"
+    .option norvc
+    .align 4
+    .globl __strapvec
+ __strapvec:
+    addi sp, sp, -256
   
-//     sd x0, 0(sp)
-//     sd x1, 8(sp)
-//     sd x2, 16(sp)
-//     sd x3, 24(sp)
-//     sd x4, 32(sp)
-//     sd x5, 40(sp)
-//     sd x6, 48(sp)
-//     sd x7, 56(sp)
-//     sd x8, 64(sp)
-//     sd x9, 72(sp)
-//     sd x10, 80(sp)
-//     sd x11, 88(sp)
-//     sd x12, 96(sp)
-//     sd x13, 104(sp)
-//     sd x14, 112(sp)
-//     sd x15, 120(sp)
-//     sd x16, 128(sp)
-//     sd x17, 136(sp)
-//     sd x18, 144(sp)
-//     sd x19, 152(sp)
-//     sd x20, 160(sp)
-//     sd x21, 168(sp)
-//     sd x22, 176(sp)
-//     sd x23, 184(sp)
-//     sd x24, 192(sp)
-//     sd x25, 200(sp)
-//     sd x26, 208(sp)
-//     sd x27, 216(sp)
-//     sd x28, 224(sp)
-//     sd x29, 232(sp)
-//     sd x30, 240(sp)
+    sd x0, 0(sp)
+    sd x1, 8(sp)
+    sd x2, 16(sp)
+    sd x3, 24(sp)
+    sd x4, 32(sp)
+    sd x5, 40(sp)
+    sd x6, 48(sp)
+    sd x7, 56(sp)
+    sd x8, 64(sp)
+    sd x9, 72(sp)
+    sd x10, 80(sp)
+    sd x11, 88(sp)
+    sd x12, 96(sp)
+    sd x13, 104(sp)
+    sd x14, 112(sp)
+    sd x15, 120(sp)
+    sd x16, 128(sp)
+    sd x17, 136(sp)
+    sd x18, 144(sp)
+    sd x19, 152(sp)
+    sd x20, 160(sp)
+    sd x21, 168(sp)
+    sd x22, 176(sp)
+    sd x23, 184(sp)
+    sd x24, 192(sp)
+    sd x25, 200(sp)
+    sd x26, 208(sp)
+    sd x27, 216(sp)
+    sd x28, 224(sp)
+    sd x29, 232(sp)
+    sd x30, 240(sp)
 
-//     call {}
+    call {}
  
-//     ld x0, 0(sp)
-//     ld x1, 8(sp)
-//     ld x2, 16(sp)
-//     ld x3, 24(sp)
-//     ld x5, 40(sp)
-//     ld x6, 48(sp)
-//     ld x7, 56(sp)
-//     ld x8, 64(sp)
-//     ld x9, 72(sp)
-//     ld x10, 80(sp)
-//     ld x11, 88(sp)
-//     ld x12, 96(sp)
-//     ld x13, 104(sp)
-//     ld x14, 112(sp)
-//     ld x15, 120(sp)
-//     ld x16, 128(sp)
-//     ld x17, 136(sp)
-//     ld x18, 144(sp)
-//     ld x19, 152(sp)
-//     ld x20, 160(sp)
-//     ld x21, 168(sp)
-//     ld x22, 176(sp)
-//     ld x23, 184(sp)
-//     ld x24, 192(sp)
-//     ld x25, 200(sp)
-//     ld x26, 208(sp)
-//     ld x27, 216(sp)
-//     ld x28, 224(sp)
-//     ld x29, 232(sp)
-//     ld x30, 240(sp)
+    ld x0, 0(sp)
+    ld x1, 8(sp)
+    ld x2, 16(sp)
+    ld x3, 24(sp)
+    ld x5, 40(sp)
+    ld x6, 48(sp)
+    ld x7, 56(sp)
+    ld x8, 64(sp)
+    ld x9, 72(sp)
+    ld x10, 80(sp)
+    ld x11, 88(sp)
+    ld x12, 96(sp)
+    ld x13, 104(sp)
+    ld x14, 112(sp)
+    ld x15, 120(sp)
+    ld x16, 128(sp)
+    ld x17, 136(sp)
+    ld x18, 144(sp)
+    ld x19, 152(sp)
+    ld x20, 160(sp)
+    ld x21, 168(sp)
+    ld x22, 176(sp)
+    ld x23, 184(sp)
+    ld x24, 192(sp)
+    ld x25, 200(sp)
+    ld x26, 208(sp)
+    ld x27, 216(sp)
+    ld x28, 224(sp)
+    ld x29, 232(sp)
+    ld x30, 240(sp)
   
-//     addi sp, sp, 256
-//     sret
-//     "#, sym handler
-//     );
+    addi sp, sp, 256
+    sret
+    "#, sym s_handler
+    );
