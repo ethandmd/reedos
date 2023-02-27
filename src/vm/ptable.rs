1a6878cd36130a65dafe4a6585c16db67ecbb0b9
ethandmd <ethandmd@protonmail.com>
2023-02-26 22:59:09 -0800

    Refactor walk() and page_map() to work with new palloc()

--- src/vm/ptable.rs
+++ src/vm/ptable.rs
@@ -1,7 +1,7 @@
 // VA: 39bits, PA: 56bits
 // PTE size = 8 bytes
 use core::assert;
-use crate::vm::{PAGEPOOL, Palloc, PallocError};
+use crate::vm::*;
 use crate::hw::param::*;
 use crate::hw::riscv::write_satp;
 
@@ -16,7 +16,7 @@ const PTE_GLOBAL: usize = 1 << 5;
 const PTE_ACCESSED: usize = 1 << 6;
 const PTE_DIRTY: usize = 1 << 7;
 
-type VirtAddress = usize;
+type VirtAddress = *mut usize;
 type PhysAddress = *mut usize;
 type PTEntry = usize;
 pub type SATPAddress = usize;
@@ -27,15 +27,9 @@ struct PageTable {
     base: PhysAddress, // Page Table located at base address.
 }
 
-#[derive(Debug)]
-enum VmError{
-    PallocFail,
-    InvalidPage,
-} // Custom error type, may remove later.
-
 macro_rules! vpn {
     ($p:expr, $l:expr) => {
-        ($p) >> (12 + 9 * $l) & 0x1FF
+        (($p).addr()) >> (12 + 9 * $l) & 0x1FF
     }
 }
 
@@ -47,7 +41,7 @@ macro_rules! PteToPhy {
 
 macro_rules! PhyToPte {
     ($p:expr) => {
-        ((($p) >> 12) << 10)
+        (((($p).addr()) >> 12) << 10)
     }
 }
 
@@ -65,7 +59,7 @@ macro_rules! PteSetFlag {
 
 macro_rules! PhyToSATP {
     ($pte:expr) => {
-        (1 << 63) | (($pte) >> 12)
+        (1 << 63) | ((($pte).addr()) >> 12)
     }
 }
 
@@ -93,46 +87,30 @@ impl PageTable {
             get_phy_offset(self.base, idx)
         }
     }
-
-    fn print_table(&self) {
-        for i in 0..PTE_TOP {
-            let pte = self.index_mut(i);
-            unsafe {
-                //if *pte != 0 {
-                print!("{}: {:?}, ", i, *pte);
-                if i > 0 && i % 4 == 0 {
-                    println!();
-                }
-                //}
-            }
-        }
-        println!();
-    }
 }
 
 // Get the address of the PTE for va given the page table pt.
 // Returns Either PTE or None, callers responsibility to use PTE 
 // or allocate a new page.
-unsafe fn walk(pt: &PageTable, va: VirtAddress, alloc_new: bool) -> Result<*mut PTEntry, PallocError> {
+unsafe fn walk(pt: &PageTable, va: VirtAddress, alloc_new: bool) -> Result<*mut PTEntry, VmError> {
     let mut table = pt.clone();
-    assert!(va < VA_TOP);
+    assert!(va.addr() < VA_TOP);
     for level in (1..3).rev() {
         let idx = vpn!(va, level);
         let next: *mut PTEntry = table.index_mut(idx);
-        println!("level {}: {:?}", level, table.base);
         table = match PteGetFlag!(*next, PTE_VALID) {
             true => { PageTable::from(*next) },
             false => {
                 if alloc_new {
-                    match (*PAGEPOOL).palloc(1) {
+                    match (*PAGEPOOL).palloc() {
                         Ok(pg) => {
-                            *next = PteSetFlag!(PhyToPte!(pg.addr.addr()), PTE_VALID);
-                            PageTable::from(PhyToPte!(pg.addr.addr()))
+                            *next = PteSetFlag!(PhyToPte!(pg.addr), PTE_VALID);
+                            PageTable::from(PhyToPte!(pg.addr))
                         }
                         Err(e) => { return Err(e) }
                     }
                 } else { 
-                    return Err(PallocError::PallocFail); 
+                    return Err(VmError::PallocFail); 
                 }
             }
         };
@@ -140,8 +118,6 @@ unsafe fn walk(pt: &PageTable, va: VirtAddress, alloc_new: bool) -> Result<*mut 
     // Last, return PTE leaf. Assuming we are all using 4K pages right now.
     // Caller's responsibility to check flags.
     let idx = vpn!(va, 0);
-    let print_table = PageTable::from(*table.index_mut(idx));
-    println!("level {}: {:?}", 0, print_table.base);
     Ok(table.index_mut(idx))
 }
 
@@ -149,24 +125,15 @@ unsafe fn walk(pt: &PageTable, va: VirtAddress, alloc_new: bool) -> Result<*mut 
 /// size. 
 ///
 /// Rounds down va and size to page size multiples. 
-// TODO: Implement VmError
 fn page_map(pt: &mut PageTable, va: VirtAddress, pa: PhysAddress, size: usize, flag: usize) -> Result<(), VmError> {
     // Round up to next page aligned boundary (multiple of pg size).
-    //let start = va & !(4096 - 1); // round page down
-    let start = va + (PAGE_SIZE - 1) & !(PAGE_SIZE - 1);
-    //let num_pages = size + (PAGE_SIZE - 1) & !(PAGE_SIZE - 1);
+    let start = va.map_addr(|addr| addr + (PAGE_SIZE - 1) & !(PAGE_SIZE - 1));
     let num_pages = ((size + (PAGE_SIZE - 1)) & !(PAGE_SIZE - 1)) >> 12;
-    if start != va {
-        log!(Warning, "Rounded va: {} up to 4K multiple: {}...", va, start);
-    }
-    if num_pages << 12 != size {
-        log!(Warning, "Rounded size, {}, up to map {} 4K pages...", size, num_pages);
-    }
-    
+   
     for i in 0..num_pages {
-        match unsafe { walk(pt, start + (4096 * i), true) } {
+        match unsafe { walk(pt, start.map_addr(|addr| addr + (4096 * i)), true) } {
             Ok(pte) => {
-                set_pte(pte, PteSetFlag!(PhyToPte!(pa as usize + PAGE_SIZE*i), flag | PTE_VALID)); 
+                set_pte(pte, PteSetFlag!(PhyToPte!(pa.map_addr(|addr| addr + PAGE_SIZE*i)), flag | PTE_VALID)); 
             },
             Err(e) => {
                 log!(Error, "{:?}", e);
@@ -180,13 +147,13 @@ fn page_map(pt: &mut PageTable, va: VirtAddress, pa: PhysAddress, size: usize, f
 
 // Initialize kernel page table 
 pub fn kpage_init() {
-    let base = unsafe { (*PAGEPOOL).palloc(1).expect("Couldn't allocate new page") };
-    log!(Debug, "Kernel page table base addr: {:#02x}", base.addr.addr());
-    let mut kpage_table = PageTable { base: base.addr };
+    let base = unsafe { (*PAGEPOOL).palloc().expect("Couldn't allocate root kernel page table.") };
+    //log!(Debug, "Kernel page table base addr: {:#02x}", base.addr.addr());
+    let mut kpage_table = PageTable { base: base.addr as *mut usize };
 
     _ = page_map(
         &mut kpage_table, 
-        UART_BASE, 
+        UART_BASE as *mut usize, 
         UART_BASE as *mut usize, 
         PAGE_SIZE, 
         PTE_READ | PTE_WRITE);
@@ -196,17 +163,18 @@ pub fn kpage_init() {
         &mut kpage_table, 
         DRAM_BASE, 
         DRAM_BASE as *mut usize, 
-        text_end().addr() - DRAM_BASE, 
+        text_end().addr() - DRAM_BASE.addr(), 
         PTE_READ | PTE_EXEC);
     log!(Debug, "Succesfully mapped kernel text into kernel pgtable...");
 
     _ = page_map(
         &mut kpage_table, 
-        text_end().addr(), 
+        text_end(), 
         text_end(), 
         dram_end().addr() - text_end().addr(), 
         PTE_READ | PTE_WRITE);
     log!(Debug, "Succesfully mapped kernel heap...");
 
-    write_satp(PhyToSATP!(kpage_table.base as usize));
+    // SFENCE.VMA Instruction here? After?
+    write_satp(PhyToSATP!(kpage_table.base));
 }
