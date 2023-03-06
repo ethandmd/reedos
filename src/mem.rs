5c641a58dbe7eb50b16fb5aec77b5dcbf1656b97
ethandmd <ethandmd@protonmail.com>
2023-03-05 23:05:10 -0800

    Add placeholder Kbox::new() fn

--- src/mem.rs
+++ src/mem.rs
@@ -1,3 +1,18 @@
-use core::ptr::NonNull;
+//! Kernel memory utilities
 
-pub struct Kbox<T: ?Sized>(NonNull<T>);
+/// Kernel heap allocated pointer
+pub struct Kbox<T: ?Sized> {
+    inner: *mut T, // NonNull<T>, try NonNull later for lazy allocation impl.
+}
+
+impl<T> Kbox<T> {
+    pub fn new(inner: T) -> Self {
+        // How the allocater interface should be made use of.
+        // let new_ptr = global allocator, allocate us size_of::<T>() bytes, please.
+        // new_ptr.write(inner); <-- initialize newly allocated memory with our inner value.
+        let new_ptr: *mut T = core::ptr::null_mut(); // Delete placeholder code.
+        Self {
+            inner: new_ptr as *mut T,
+        }
+    }
+}
