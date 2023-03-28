5603380fb379f73d9c47e0d00441862cfac49959
ethandmd <ethandmd@protonmail.com>
2023-03-27 23:08:26 -0700

    Add sifive_u spec comment

--- src/hw/param.rs
+++ src/hw/param.rs
@@ -1,29 +1,33 @@
 //! System parameters and memory layout.
-// Qemu riscv virt machine memory locations you want to know:
-// https://github.com/qemu/qemu/blob/master/hw/riscv/virt.c
 //
-//static static MemMapEntry virt_memmap[] = {
-//    [VIRT_DEBUG] =        {        0x0,         0x100 },
-//    [VIRT_MROM] =         {     0x1000,        0xf000 },
-//    [VIRT_TEST] =         {   0x100000,        0x1000 },
-//    [VIRT_RTC] =          {   0x101000,        0x1000 },
-//    [VIRT_CLINT] =        {  0x2000000,       0x10000 },
-//    [VIRT_ACLINT_SSWI] =  {  0x2F00000,        0x4000 },
-//    [VIRT_PCIE_PIO] =     {  0x3000000,       0x10000 },
-//    [VIRT_PLATFORM_BUS] = {  0x4000000,     0x2000000 },
-//    [VIRT_PLIC] =         {  0xc000000, VIRT_PLIC_SIZE(VIRT_CPUS_MAX * 2) },
-//    [VIRT_APLIC_M] =      {  0xc000000, APLIC_SIZE(VIRT_CPUS_MAX) },
-//    [VIRT_APLIC_S] =      {  0xd000000, APLIC_SIZE(VIRT_CPUS_MAX) },
-//    [VIRT_UART0] =        { 0x10000000,         0x100 },
-//    [VIRT_VIRTIO] =       { 0x10001000,        0x1000 },
-//    [VIRT_FW_CFG] =       { 0x10100000,          0x18 },
-//    [VIRT_FLASH] =        { 0x20000000,     0x4000000 },
-//    [VIRT_IMSIC_M] =      { 0x24000000, VIRT_IMSIC_MAX_SIZE },
-//    [VIRT_IMSIC_S] =      { 0x28000000, VIRT_IMSIC_MAX_SIZE },
-//    [VIRT_PCIE_ECAM] =    { 0x30000000,    0x10000000 },
-//    [VIRT_PCIE_MMIO] =    { 0x40000000,    0x40000000 },
-//    [VIRT_DRAM] =         { 0x80000000,           0x0 },
-//}
+// Qemu riscv sifive_u machine: https://github.com/qemu/qemu/blob/master/hw/riscv/sifive_u.c
+// Devices: UART, CLINT, PLIC, PRCI, GPIO, OTP, GEM (ethernet), DMA, SPIO, SPI2, PWM{0,1}
+// /* CLINT timebase frequency */
+// #define CLINT_TIMEBASE_FREQ 1000000
+
+//static const MemMapEntry sifive_u_memmap[] = {
+//    [SIFIVE_U_DEV_DEBUG] =    {        0x0,      0x100 },
+//    [SIFIVE_U_DEV_MROM] =     {     0x1000,     0xf000 },
+//    [SIFIVE_U_DEV_CLINT] =    {  0x2000000,    0x10000 },
+//    [SIFIVE_U_DEV_L2CC] =     {  0x2010000,     0x1000 },
+//    [SIFIVE_U_DEV_PDMA] =     {  0x3000000,   0x100000 },
+//    [SIFIVE_U_DEV_L2LIM] =    {  0x8000000,  0x2000000 },
+//    [SIFIVE_U_DEV_PLIC] =     {  0xc000000,  0x4000000 },
+//    [SIFIVE_U_DEV_PRCI] =     { 0x10000000,     0x1000 },
+//    [SIFIVE_U_DEV_UART0] =    { 0x10010000,     0x1000 },
+//    [SIFIVE_U_DEV_UART1] =    { 0x10011000,     0x1000 },
+//    [SIFIVE_U_DEV_PWM0] =     { 0x10020000,     0x1000 },
+//    [SIFIVE_U_DEV_PWM1] =     { 0x10021000,     0x1000 },
+//    [SIFIVE_U_DEV_QSPI0] =    { 0x10040000,     0x1000 },
+//    [SIFIVE_U_DEV_QSPI2] =    { 0x10050000,     0x1000 },
+//    [SIFIVE_U_DEV_GPIO] =     { 0x10060000,     0x1000 },
+//    [SIFIVE_U_DEV_OTP] =      { 0x10070000,     0x1000 },
+//    [SIFIVE_U_DEV_GEM] =      { 0x10090000,     0x2000 },
+//    [SIFIVE_U_DEV_GEM_MGMT] = { 0x100a0000,     0x1000 },
+//    [SIFIVE_U_DEV_DMC] =      { 0x100b0000,    0x10000 },
+//    [SIFIVE_U_DEV_FLASH0] =   { 0x20000000, 0x10000000 },
+//    [SIFIVE_U_DEV_DRAM] =     { 0x80000000,        0x0 },
+//};
 
 use core::ptr::addr_of_mut;
 
@@ -51,7 +55,7 @@ extern "C" {
 pub const CLINT_BASE: usize = 0x2000000;
 
 /// UART base adderss.
-pub const UART_BASE: usize = 0x10000000;
+pub const UART0_BASE: usize = 0x10010000;
 
 /// Start of kernel memory (first .text section goes here).
 pub const DRAM_BASE: *mut usize = 0x80000000 as *mut usize;
