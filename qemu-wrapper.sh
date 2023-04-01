6507bd0c6b42f80091526d3e0f14c69023fbbbec
Dylan McNamee <dylan.mcnamee@gmail.com>
2023-04-01 14:27:54 -0700

    macOS uses old bash...this change should work on both Linux and macOS

--- qemu-wrapper.sh
+++ qemu-wrapper.sh
@@ -12,7 +12,7 @@ FLAGS=(-machine virt -smp 2 -m 128M -bios none -nographic)
 print_help() { echo "$(tput setaf 2)$(tput bold)(info)$(tput sgr0) $1"; }
 
 print_help "Type CTRL-A, X to exit QEMU"
-if [[ -v DEBUG ]]; then
+if [ -n "${DEBUG+x}" ] ; then
     FLAGS+=(-s -S)
     print_help "Starting QEMU in debug mode (connect with gdb)"
 fi
