a883a975e13f0c206bf5e598dd7888692c3ddf0c
tmu <t.man.atl.2012@gmail.com>
2023-04-16 18:40:54 -0700

    spin-based condition variable

---
+++ src/lock/condition.rs
@@ -0,0 +1,25 @@
+//! Condition variable implementation
+//!
+//! Spins and does not block, used outside and around scheduling
+
+use core::sync::atomic::{Ordering, AtomicUsize};
+use core::hint::spin_loop;
+
+pub struct ConditionVar {
+    contents: AtomicUsize,
+}
+
+impl ConditionVar {
+    pub fn new(val: usize) -> Self {
+        Self {
+            contents: AtomicUsize::new(val)
+        }
+    }
+
+    pub fn spin_wait(&self, expected: usize) {
+        while self.contents.load(Ordering::Acquire) != expected {
+            spin_loop();
+        }
+    }
+}
+
