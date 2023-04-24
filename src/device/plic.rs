b3ccdbbc10ed2d0f3681813e3c90e064be9e9a18
John M. Poole <jmp@jump-high-686.local>
2023-04-24 14:46:05 -0700

    wrote stub for PLIC. bugged

---
+++ src/device/plic.rs
@@ -0,0 +1,147 @@
+// PLIC device. There is usually only one. It should be locked probably.
+
+// The PLIC mediates S-Mode and M-Mode external interrupts across all Harts.
+// The interface exists at a known memory location: PLIC_BASE
+
+// When a device asks for an interrupt, it provides its priority level.
+// If a device's interrupt IRQ-value is above a threshold, the PLIC
+// serves its interrupt. Each Hart has a distinct threshold encoded at 
+// a fixed offset and step from its HartID. Serving interrupts, the PLIC
+// device gives you the highest priority pending interrupt. 
+// The claim function sends a read-signal to an mmapped register.
+// The complete function sends a write-signal to the mmapped register.
+
+// REFS: https://github.com/riscv/riscv-plic-spec/blob/master/riscv-plic.adoc#interrupt-priorities
+// https://github.com/Pooletergeist/reedos/blob/40-move-to-qemu-sifive-unleashed/src/device/plic.rs
+// https://github.com/sgmarz/osblog/blob/master/risc_v/src/plic.rs (a little)
+    // could use it more.. e.g. cases more rustlike.
+
+use core::mem::{MaybeUninit};
+use core::cell::OnceCell; // for PLIC, write once read many times
+//use core::ops::Deref;
+
+use crate::hw::riscv;
+// use crate::lock::mutex::*;
+use crate::hw::param::{PLIC_BASE, UART_IRQ};
+// ^ constants for PLIC_BASE & device interrupt (IRQ) priority locations.
+
+pub static mut PLIC: MaybeUninit<OnceCell<Plic>> = MaybeUninit::uninit(); // all memory accesses to Plic go through here!
+
+pub struct Plic { 
+    base: usize,
+}
+
+/// Single-time global initialization for Plic.
+/// Sets magic number device IRQ priorities, then initializes.
+pub fn global_init() {
+    // set desired IRQ priorities non-zero (otherwise disabled).
+    // currently just for UART
+    let base_addr = PLIC_BASE as *mut u32;
+
+    unsafe {
+        base_addr.add(UART_IRQ*4).write_volatile(1);
+    }
+
+    // initialize PLIC
+    unsafe {
+        PLIC.write(OnceCell::new());
+        PLIC.assume_init().set(Plic::new(PLIC_BASE));
+    }
+}
+
+/// Local initialize Plic. Once per HART. Enables devices, sets threshold.
+pub fn local_init() {
+
+    //  set enable bits for this hart's S-mode
+    // currently just for Uart
+    let bit_mask: u32 = 1 << UART_IRQ;
+
+    unsafe {
+    PLIC.assume_init().get().unwrap().hart_local_enable(bit_mask); 
+    }
+    
+    let initialized_plic = unsafe{PLIC.assume_init()}; // would this allow for copies of PLIC that circumvented the mutex?
+
+    // call the write to Plic magic locations for the enabled bits.
+    initialized_plic.get().unwrap().hart_local_enable(bit_mask);
+    // accept interrupts from all enabled devices with priority > 0.
+    initialized_plic.get().unwrap().set_s_priority_threshold(0);
+
+    // ASK: does MUTEX drop when locked_plic goes out of scope here? If so, why is there a drop method?
+}
+
+// currently stolen directly from xv6-riscv
+
+/// new makes Plic at a specific place with specific interrupts enabled.
+impl Plic {
+    fn new(base: usize) -> Self {
+        // new Plic at base
+        let out = Plic {
+            base,
+        };
+
+        out
+    }
+
+    /// take a Plic and a threshold, set threshold at a magic location
+    /// known explicitly per hart: (RAW_OFFSET + hart * RAW_STEP). 
+    fn set_s_priority_threshold(&self, threshold: u32) {
+        let addr = self.base as *mut u32;
+        let hart = riscv::read_tp() as usize;
+
+        const RAW_OFFSET: usize = 0x201000;
+        const RAW_STEP: usize = 0x2000;
+
+        let final_offset = (RAW_OFFSET + (hart * RAW_STEP)); // / 4?
+        unsafe {
+            addr.add(final_offset).write_volatile(threshold);
+        }
+    }
+
+    /// enables interrupts from devices by non-zero bits in bit-mask
+    pub fn hart_local_enable(&self, bit_mask: u32) {
+        let addr = self.base as *mut u32;
+        let hart = riscv::read_tp() as usize;
+        
+        const RAW_OFFSET: usize = 0x2080;
+        const RAW_STEP: usize = 0x100;
+        
+        let final_offset = (RAW_OFFSET + (hart * RAW_STEP)); //ASK: /4?
+        
+        unsafe {
+            addr.add(final_offset).write_volatile(bit_mask); //ASK: length?
+        }  
+    } 
+
+    /// Claim an interupt that you were alerted to.
+    pub fn claim(&mut self) -> u32 {
+        let addr = self.base as *mut u32;
+        let hart = riscv::read_tp() as usize;
+
+        const RAW_OFFSET: usize = 0x201004; // 4-bits after threshold
+        const RAW_STEP: usize = 0x2000;
+
+        let final_offset = (RAW_OFFSET + (hart * RAW_STEP)); //ASK / 4?
+        unsafe {
+            // returns highest-priority pending interrupt
+            addr.add(final_offset).read_volatile()
+            // ^ reading mmapped register
+        }
+    }
+
+    /// Alert the PLIC that we have completed the interupt we claimed
+    pub fn complete(&mut self, irq: u32) {
+        let addr = self.base as *mut u32;
+        let hart = riscv::read_tp() as usize;
+
+        const RAW_OFFSET: usize = 0x201004;
+        const RAW_STEP: usize = 0x2000;
+
+        let final_offset = (RAW_OFFSET + (hart * RAW_STEP)); //ASK: / 4?
+        unsafe {
+            // signals completion of interrupt identified by IRQ
+            addr.add(final_offset).write_volatile(irq);
+            // ^ writing mmapped register
+        }
+    }
+}
