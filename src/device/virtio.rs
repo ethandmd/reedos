4f02be68ac39d5e79ec23a11db90a4c96a343a87
ethandmd <ethandmd@protonmail.com>
2023-04-30 15:37:28 -0700

    Add stand in virtio init

--- src/device/virtio.rs
+++ src/device/virtio.rs
@@ -1,6 +1,8 @@
 //! Access the virtio device through the mmio interface provided by QEMU.
 //! [Virtual I/O Device (VIRTIO) Specs](https://docs.oasis-open.org/virtio/virtio/v1.1/virtio-v1.1.html)
 
+// Also a nice walkthrough: https://www.redhat.com/en/blog/virtio-devices-and-drivers-overview-headjack-and-phone
+
 use crate::alloc::vec::Vec;
 
 // Also checkout: https://wiki.osdev.org/Virtio
@@ -11,7 +13,7 @@ use crate::alloc::vec::Vec;
 // discovery, we will have to know exactly where in memory the virtio device is.
 // Assume that we are only interested in virtio-mmio. These values are not valid for
 // other virtio transport options (over PCI bus, channel I/O).
-const VIRIO_BASE: usize = 0x10001000; // From hw/params.rs
+const VIRTIO_BASE: usize = 0x10001000; // From hw/params.rs
 const VIRTIO_MAGIC: usize = 0x0; //0x74726976 := Little endian equiv to "virt" string.
 const VIRTIO_VERSION: usize = 0x004; // Device version number is 0x2, legace 0x1.
 const VIRTIO_DEVICE_ID: usize = 0x008; // c.f. https://docs.oasis-open.org/virtio/virtio/v1.1/virtio-v1.1.pdf#b7
@@ -59,12 +61,12 @@ enum VirtioDeviceFeatures {}
 
 // VirtQueues; Section 2.5.
 // 
-// Based on legacy and splitqueue: Section 2.6.
-// Everything is a vector until we know what we need.
-struct VirtQueueLegacy {
+// Based on (legacy) splitqueue: Section 2.6.
+// Device versions <= 0x1 only have split queue.
+struct SplitVirtQueue {
     num: u32,
     // Descriptor Area: describe buffers (make fixed array?)
-    desc: Vec<VirtQueueDescTable>,
+    desc: Vec<VirtQueueDesc>,
     // Driver Area (aka Available ring): extra info from driver to device
     avail: VirtQueueAvailable,
     // Device Area (aka Used ring): extra info from device to driver
@@ -75,33 +77,39 @@ struct VirtQueueLegacy {
 
 // VirtQueue Descriptor Table; Section 2.6.5.
 // Everything little endian.
+// * If flag is empty => read-only buffer. *
 enum VirtQueueDescFeat {
-    Next = 1,       // Buffer continues into NEXT field.
-    Write = 2,      // Buffer as device write-only (otherwise device read-only).
-    Indirect = 4,   // Buffer contains a list of buffer descriptors.
+    Next = 0x1,       // Buffer continues into NEXT field.
+    Write = 0x2,      // Buffer as device write-only.
+    Indirect = 0x4,   // Buffer contains a list of buffer descriptors.
 }
 
-struct VirtQueueDescTable {
-    addr: usize,
+// Note that we don't need IOMMU since this is all in QEMU process.
+// If this were a real physical device, then we need IOMMU.
+struct VirtQueueDesc {
+    addr: u64, // Specifically little endian 64
     len: u32,
     flags: u16,
     next: u16,
 }
 
+const RING_SIZE: usize = 2;
 // Section 2.6.6
+// ** Ring queue size is power of 2 and avail, used
+// queues should be same size.
 struct VirtQueueAvailable {
-    flags: u16,
-    idx: u16,
-    ring: Vec<u16>, // Length := numb o chain heads
-    used_event: u16, // Only if feature event index is set.
+    flags: u16,             // LSB := VIRTQ_AVAIL_F_NO_INTERRUPT
+    idx: u16,               // Where driver puts next desc entry % queue size.
+    ring: [u16; RING_SIZE],  // Length := numb o chain heads
+    used_event: u16,        // Only if feature EVENT_INDEX is set.
 }
 
 // Section 2.6.8
 struct VirtQueueUsed {
     flags: u16,
     idx: u16,
-    used_ring: Vec<VirtQueueUsedElem>,
-    avail_event: u16, // Onlly if feature event index is set.
+    used_ring: [VirtQueueUsedElem; RING_SIZE],
+    avail_event: u16, // Only if feature EVENT_INDEX is set.
 }
 
 struct VirtQueueUsedElem {
@@ -109,3 +117,17 @@ struct VirtQueueUsedElem {
     len: u32,
 }
 // Device Initialization: Sections 3 (general) + 4.2.3 (mmio)
+// Currently testing this QEMU board device:
+pub unsafe fn test_virtio_init() {
+    //let base = VIRTIO_BASE as *mut u8;
+    // Step 0: Read device info.
+    let magic = ((VIRTIO_BASE + VIRTIO_MAGIC) as *mut u32).read_volatile();
+    let ver = ((VIRTIO_BASE + VIRTIO_VERSION) as *mut u32).read_volatile();
+    let dev_id = ((VIRTIO_BASE + VIRTIO_DEVICE_ID) as *mut u32).read_volatile();
+    let ven_id = ((VIRTIO_BASE + VIRTIO_VENDOR_ID) as *mut u32).read_volatile();
+    println!("VIRTIO DEVICE INFO:\n \
+    magic: {:#02x}\n \
+    version: {:#02x}; Want: 0x2\n \
+    device ID: {:#02x}\n \
+    vendor ID: {:#02x}", magic, ver, dev_id, ven_id);
+}
