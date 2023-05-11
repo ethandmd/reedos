ee763b2a263fd2440a02af3447655aa9f75d3b81
ethandmd <ethandmd@protonmail.com>
2023-05-11 09:01:47 -0700

    Set ext2 block size at 1024 B

--- src/fs.rs
+++ src/fs.rs
@@ -1,5 +1,8 @@
 pub mod structs;
 
+/// EXT2 Block Size can be 1k, 2k, 4k, 8k
+pub const EXT2_BLOCK_SIZE: u64 = 1024;
+
 const EXT2_MAGIC: u16 = 0xef53;
 const EXT2_START_OF_SUPERBLOCK: usize = 1024;
 const EXT2_END_OF_SUPERBLOCK: usize = 2048;
