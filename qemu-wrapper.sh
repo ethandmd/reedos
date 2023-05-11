2431aea0bdd2297e9453866cd509029dd3799471
ethandmd <ethandmd@protonmail.com>
2023-05-11 06:29:46 -0700

    Add shadow commands for building ext2 fs.img

--- qemu-wrapper.sh
+++ qemu-wrapper.sh
@@ -7,6 +7,13 @@
 
 set -euo pipefail
 
+##just creates a 'raw' ext2 fs on fs.img. No partitioning or anything, like so:
+##fs.img: Linux rev 1.0 ext2 filesystem data, UUID=0a160ba6-2a1f-4013-8d54-5876e1c8697d (large files)
+##do:
+##qemu-img create fs.img -f raw 16M
+##then
+##mkfs.ext2 fs.img
+
 # ** Don't forget to `$qemu-img create fs.img 64k` (or whatever size you want).
 FLAGS=(-machine virt -smp 2 -m 128M -bios none -nographic \
     -global virtio-mmio.force-legacy=false \
