8a58f5792bce777a8f9db49532405b3502ecc911
ethandmd <ethandmd@protonmail.com>
2023-05-15 13:19:45 -0700

    makefile wrapper now always creates new ext2 fs.image

--- qemu-wrapper.sh
+++ qemu-wrapper.sh
@@ -9,11 +9,16 @@ set -euo pipefail
 
 ## Creates a 'raw' ext2 fs on fs.img file.
 ##
-## dd if=/dev/zero of=fs.img bs=512 count=2097152 # 1G file!
+dd if=/dev/zero of=fs.img bs=512 count=524288 # 256MB file
 ##
 ## Specify 4k block size, check fs.img for bad block, and optionally
-## copy /usr/bin (e.g. builtin programs) to the filesystem.
-## mkfs.ext2 -b 4096 -c fs.img # -d /usr/bin
+## copy root dir contents into to the filesystem root dir.
+make -C ./src/programs/spin
+make -C ./src/programs/syscall-basic
+mkdir tmp
+find ./src/programs -name '*.elf' -exec cp {} ./tmp \;
+mkfs.ext2 -b 4096 -c fs.img  -d ./tmp
+rm -rf ./tmp
 
 # ** Don't forget to `$qemu-img create fs.img 64k` (or whatever size you want).
 FLAGS=(-machine virt -smp 2 -m 128M -bios none -nographic \
